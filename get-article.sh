de63b936c13b404b0cf3ba3df44ca8b59abc92ea
PigCatDog <PigCatDog@protonmail.com>
2024-07-15 04:06:38 +0200

    women up to 2023.12.01 & mangmang 20240713

---
+++ get-article.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check numbers of arg
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 --[convert|images] <dir>"
+    exit 1
+fi
+
+case $1 in
+    --convert)
+        pandoc -s $2.html -o $2.org --wrap=none
+        ;;
+    --images)
+	rm -rf $2
+	mv $2-Dateien $2
+	grep "file:.*\.\(png\|jpg\|jpeg\|JPG\)" $2.org | sed -e 's/\[\[//' -e 's/\]\]//' -e 's/file://' | sed ':a;N;$!ba;s/\n/ /g' | xclip
+        mkdir -p build
+	mv `xclip -o` build
+	rm -r $2
+	mv build $2
+        ;;
+    *)
+        # something
+        echo "Invalid option: $1"
+        exit 1
+        ;;
+esac
